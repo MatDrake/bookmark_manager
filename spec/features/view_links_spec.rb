6cc443af47b6efca134633da9021aa5b3ba02427
Matthew Drake <matdrake@live.co.uk>
2016-02-09 17:49:16 +0000

    Incorporated database_cleaner into RSpec to allow for more reliable and cleaner tests

--- spec/features/view_links_spec.rb
+++ spec/features/view_links_spec.rb
@@ -1,16 +1,8 @@
 require_relative '../spec_helper'
 
 feature 'Links' do
-  Link.all.destroy
-  scenario 'add links'do
-    visit('/links/new')
-    fill_in('name', with: 'Google')
-    fill_in('link', with: 'www.google.co.uk')
-    click_button('save')
-    expect(page).to have_content('Link Saved!')
-  end
-  
-  scenario 'can be viewed' do
+  scenario 'can be saved and viewed' do
+    save_link
     visit('/links')
     expect(page).to have_content 'Google'
     expect(page).to have_content 'www.google.co.uk'
